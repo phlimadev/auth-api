b9b39f52d8e0fba634396d8e2c60ee8c4bc77432
phlimadev <devphlima@gmail.com>
2025-01-30 08:41:14 -0300

    implementing spring security

--- src/main/java/com/example/auth_api/entities/User.java
+++ src/main/java/com/example/auth_api/entities/User.java
@@ -2,6 +2,12 @@ package com.example.auth_api.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -10,10 +16,43 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String login;
     private String password;
     private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_COMMON"));
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_COMMON"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/example/auth_api/repositories/UserRepository.java
+++ src/main/java/com/example/auth_api/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.auth_api.repositories;
 
 import com.example.auth_api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
 }
